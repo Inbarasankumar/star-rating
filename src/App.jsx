445c1f2ae701dc921a4e5aee67dae210eb9a7e08
Inbarasan <inbarasanraja@gmail.com>
2025-03-28 10:49:57 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
+import StarRating from "./StarRating"
 
 function App() {
 
   return (
-    <div>
-      <div className="text-3xl text-center mt-20">
-        Simple React 19 + Vite + Tailwind configured template.
-      </div>
-      <p className="text-center">No need to clear all the logo files,assets and css code. Happy Learning 😊</p>
+    <div className="bg-amber-400  w-screen h-screen">
+      <StarRating/>
     </div>
   )
 }
